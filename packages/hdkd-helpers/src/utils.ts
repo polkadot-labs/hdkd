df2d5827cb63ca54ad448670f416a8f77398f423
Matias <kratico@users.noreply.github.com>
2023-12-15 15:33:16 -0300

    feat: flag deriveCodec as pure

--- packages/hdkd-helpers/src/utils.ts
+++ packages/hdkd-helpers/src/utils.ts
@@ -5,7 +5,7 @@ import { DeriveKeyPairFn, Hex } from "./types"
 
 export const blake2b256 = (msg: Hex) => blake2b(msg, { dkLen: 32 })
 
-const derivationCodec = Tuple(str, Bytes(32), Bytes(32))
+const derivationCodec = /* @__PURE__ */ Tuple(str, Bytes(32), Bytes(32))
 const createDeriveFn =
   (prefix: string) => (seed: Uint8Array, chainCode: Uint8Array) =>
     blake2b256(derivationCodec.enc([prefix, seed, chainCode]))
