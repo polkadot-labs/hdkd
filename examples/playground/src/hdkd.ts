c5e02c32c23ba39f915cbc21c08c22ef54ef4665
Matias <kratico@users.noreply.github.com>
2023-12-15 16:00:48 -0300

    feat(playground): remove esbuild

--- examples/playground/src/hdkd.ts
+++ examples/playground/src/hdkd.ts
@@ -1,9 +1,19 @@
 import { bytesToHex } from "@noble/hashes/utils"
 import { createSr25519Derive } from "@polkadot-labs/hdkd"
-import { DEV_MINI_SECRET } from "@polkadot-labs/hdkd-helpers"
+import { sr25519, DEV_PHRASE } from "@polkadot-labs/hdkd-helpers"
+import { mnemonicToMiniSecret } from "@polkadot/util-crypto"
 
-const derive = createSr25519Derive(DEV_MINI_SECRET)
+const miniSecret = mnemonicToMiniSecret(DEV_PHRASE)
+const derive = createSr25519Derive(miniSecret)
 
 const keypair = derive("//Alice")
+const message = new TextEncoder().encode("Hello")
+const signature = keypair.sign(message)
 
-console.log("//Alice", bytesToHex(keypair.publicKey))
+console.log("//Alice")
+console.log("publicKey =", bytesToHex(keypair.publicKey))
+console.log("sign('Hello') =", bytesToHex(signature))
+console.log(
+  "verify(sign('Hello')) =",
+  sr25519.verify(signature, message, keypair.publicKey),
+)
