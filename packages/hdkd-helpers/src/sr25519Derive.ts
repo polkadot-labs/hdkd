f15386746fd206721640a6d26af2334f7dcb9fb7
Matias Volpe <kratico@users.noreply.github.com>
2025-06-20 16:12:01 -0300

    feat: use @scure/sr25519

--- packages/hdkd-helpers/src/sr25519Derive.ts
+++ packages/hdkd-helpers/src/sr25519Derive.ts
@@ -1,15 +1,11 @@
-import { randomBytes } from "@noble/hashes/utils"
-import { HDKD, getPublicKey, secretFromSeed } from "micro-sr25519"
+import { HDKD, getPublicKey, secretFromSeed } from "@scure/sr25519"
 import type { DeriveKeyPairFn } from "./internal/types"
 
 export const sr25519Derive: DeriveKeyPairFn = (seed, curve, derivations) => {
-  const privateKey = derivations.reduce(
-    (secretKey, [type, chainCode]) =>
-      type === "hard"
-        ? HDKD.secretHard(secretKey, chainCode)
-        : HDKD.secretSoft(secretKey, chainCode, randomBytes),
-    secretFromSeed(seed),
-  )
+  const privateKey = derivations.reduce((secretKey, [type, chainCode]) => {
+    const deriveFn = type === "hard" ? HDKD.secretHard : HDKD.secretSoft
+    return deriveFn(secretKey, chainCode)
+  }, secretFromSeed(seed))
   const publicKey = getPublicKey(privateKey)
   return {
     publicKey,
