12ad76a2095ac9a072f779ecdb9a03b5e4ac574c
Matias Volpe <kratico@users.noreply.github.com>
2025-08-30 16:53:12 -0300

    feat: update ecdsa sign/verify implementation

--- packages/hdkd-helpers/src/ed25519.test.ts
+++ packages/hdkd-helpers/src/ed25519.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from "vitest"
 import { ed25519 } from "./ed25519"
 
 test("ed25519", () => {
-  const privateKey = ed25519_.utils.randomPrivateKey()
+  const privateKey = ed25519_.utils.randomSecretKey()
 
   const publicKey = ed25519.getPublicKey(privateKey)
   const msg = new TextEncoder().encode("hello")
--- packages/hdkd-helpers/src/utils.ts
+++ packages/hdkd-helpers/src/utils.ts
@@ -1,5 +1,5 @@
-import { ensureBytes as ensureBytes_ } from "@noble/curves/utils.js"
 import { blake2b } from "@noble/hashes/blake2"
+import { hexToBytes, isBytes } from "@noble/hashes/utils.js"
 import { Bytes, str, Tuple } from "scale-ts"
 import type { DeriveKeyPairFn } from "./internal/types"
 import type { Hex } from "./types"
@@ -9,13 +9,41 @@ export const blake2b256 = (msg: Hex) =>
 export const blake2b512 = (msg: Hex) =>
   blake2b(ensureBytes("msg", msg), { dkLen: 64 })
 
+/**
+ * Adapted from: https://github.com/paulmillr/noble-curves/blob/a0ac59846ee76c52f7c18886f4963e1211345d48/src/utils.ts#L104 (MIT License)
+ *
+ * Takes hex string or Uint8Array, converts to Uint8Array.
+ * Validates output length.
+ * Will throw error for other types.
+ * @param title descriptive title for an error e.g. 'secret key'
+ * @param hex hex string or Uint8Array
+ * @param expectedLength optional, will compare to result array's length
+ * @returns
+ */
 export const ensureBytes = (
   title: string,
   hex: Hex,
   expectedLength?: number,
 ) => {
-  if (typeof hex === "string" && hex[1] === "x") hex = hex.slice(2)
-  return ensureBytes_(title, hex, expectedLength)
+  let res: Uint8Array
+  if (typeof hex === "string") {
+    try {
+      if (hex[1] === "x") hex = hex.slice(2)
+      res = hexToBytes(hex)
+    } catch (e) {
+      throw new Error(`${title} must be hex string or Uint8Array, cause: ${e}`)
+    }
+  } else if (isBytes(hex)) {
+    // Uint8Array.from() instead of hash.slice() because node.js Buffer
+    // is instance of Uint8Array, and its slice() creates **mutable** copy
+    res = Uint8Array.from(hex)
+  } else {
+    throw new Error(`${title} must be hex string or Uint8Array`)
+  }
+  const len = res.length
+  if (typeof expectedLength === "number" && len !== expectedLength)
+    throw new Error(`${title} of length ${expectedLength} expected, got ${len}`)
+  return res
 }
 
 const derivationCodec = /* @__PURE__ */ Tuple(str, Bytes(32), Bytes(32))
